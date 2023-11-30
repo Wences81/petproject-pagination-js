dc68eb1e5d716f225cca3816afe8bd3f1eea1ff7
Wences81 <zhylinpochta@gmail.com>
2023-11-30 13:56:30 +0100

    30/112023

---
+++ js/app.js
@@ -0,0 +1,34 @@
+async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    return data;
+}
+
+async function main() {
+    const postsData = await getData();
+    let currentPage = 1;
+    let rows = 10;
+
+    function displayList(arrData, rowPerPage, page) {
+        const postsEl = document.querySelector('.posts');
+
+        const start = rowPerPage * page;
+        const end = start + rowPerPage;
+        const paginatedData = arrData.slice(start, end);
+
+        paginatedData.forEach((el) => {
+            const postEl = document.createElement('div');
+            postEl.classList.add('post');
+            postEl.innerText = `${el.title}`;
+            postsEl.appendChild(postEl);
+        })
+
+    }
+    function displayPagination() { }
+    function displayPaginationBtn() { }
+    
+    displayList(postsData, rows, currentPage);
+
+}
+
+main();
